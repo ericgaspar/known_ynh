f365eaefa2a14000c731a4664148aa31782bc3e2
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-01-26 10:09:32 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,10 +8,10 @@
 
 YNH_PHP_VERSION=7.4
 
-php_dependencies="php$YNH_PHP_VERSION-curl php$YNH_PHP_VERSION-dom php$YNH_PHP_VERSION-exif php$YNH_PHP_VERSION-gd php$YNH_PHP_VERSION-mbstring php$YNH_PHP_VERSION-mysql"
+php_dependencies="php$YNH_PHP_VERSION-curl php$YNH_PHP_VERSION-dom php$YNH_PHP_VERSION-exif php$YNH_PHP_VERSION-gd php$YNH_PHP_VERSION-mbstring php$YNH_PHP_VERSION-pgsql"
 
 # dependencies used by the app (must be on a single line)
-pkg_dependencies="$php_dependencies"
+pkg_dependencies="postgresql $php_dependencies"
 
 YNH_COMPOSER_VERSION=2.5.1
 
