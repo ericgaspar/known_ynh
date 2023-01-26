8a656a82d9508efd86280b6e62860ff9e175b963
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-01-26 09:24:13 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,7 +8,7 @@
 
 YNH_PHP_VERSION=8.1
 
-php_dependencies="php$YNH_PHP_VERSION-curl php$YNH_PHP_VERSION-date php$YNH_PHP_VERSION-dom php$YNH_PHP_VERSION-exif php$YNH_PHP_VERSION-gd php$YNH_PHP_VERSION-mbstring php$YNH_PHP_VERSION-mysql"
+php_dependencies="php$YNH_PHP_VERSION-curl php$YNH_PHP_VERSION-dom php$YNH_PHP_VERSION-exif php$YNH_PHP_VERSION-gd php$YNH_PHP_VERSION-mbstring php$YNH_PHP_VERSION-mysql"
 
 # dependencies used by the app (must be on a single line)
 pkg_dependencies="$php_dependencies"
