b5d296f4d0831bb9b4bf7a967bc46f23576f5eff
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-01-26 09:18:03 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,23 +5,13 @@
 #=================================================
 # PHP APP SPECIFIC
 #=================================================
-# Depending on its version, YunoHost uses different default PHP version:
-## YunoHost version "11.X" => PHP 7.4
-## YunoHost version "4.X"  => PHP 7.3
-#
-# This behaviour can be overridden by setting the YNH_PHP_VERSION variable
-#YNH_PHP_VERSION=7.3
-#YNH_PHP_VERSION=7.4
-#YNH_PHP_VERSION=8.0
-# For more information, see the PHP application helper: https://github.com/YunoHost/yunohost/blob/dev/helpers/php#L3-L6
-# Or this app package depending on PHP: https://github.com/YunoHost-Apps/grav_ynh/blob/master/scripts/_common.sh
-# PHP dependencies used by the app (must be on a single line)
-#php_dependencies="php$YNH_PHP_VERSION-deb1 php$YNH_PHP_VERSION-deb2"
-# or, if you do not need a custom YNH_PHP_VERSION:
-php_dependencies="php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
+
+YNH_PHP_VERSION=8.1
+
+php_dependencies="php$YNH_PHP_VERSION-curl php$YNH_PHP_VERSION-date php$YNH_PHP_VERSION-dom php$YNH_PHP_VERSION-exif php$YNH_PHP_VERSION-gd php$YNH_PHP_VERSION-libxml php$YNH_PHP_VERSION-mbstring php$YNH_PHP_VERSION-mysql php$YNH_PHP_VERSION-date"
 
 # dependencies used by the app (must be on a single line)
-pkg_dependencies="deb1 deb2 $php_dependencies"
+pkg_dependencies="$php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
